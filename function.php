a3d07124cd8a5cde3de5766f80230c6ce6083362
Fadhil Syauqy <rahmatfadhilsauqy08@gmail.com>
2024-05-21 07:27:43 +0700

    update

--- function.php
+++ function.php
@@ -1,11 +1,3 @@
-<!-- <?php
-
-        // $koneksi = mysqli_connect("localhost", "root", "", "sumbar_gempa");
-        // if (mysqli_connect_errno()) {
-        //     echo "Koneksi database mysqli gagal!!! : " . mysqli_connect_error();
-        // }
-        //mysqli_select_db($name, $koneksi) or die("Tidak ada database yang dipilih!");
-        ?> -->
 
 <?php
 //koneksi ke database
